d15b1103f79774038e4fa70686a55f4a9772c5a1
LeeMclafferty <lee.mclafferty@gmail.com>
2023-06-22 22:52:12 -0700

    Rendering colors in window
        - Setup a color buffer to set the entire window color that is passed to an SDL_Texture to be rendered on. As of right now this is a
        manual loop over the color buffer, but will change.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL.h>
+
+bool is_running = false;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-bool is_running = false;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		fprintf(stderr, "Error initializing SDL. \n");
-		return false;
-	}
+		return false;	}
 
 	// Create SDL Window
-	window = SDL_CreateWindow("CPU Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+	window = SDL_CreateWindow("CPU Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_RESIZABLE);
 	if (!window)
 	{
 		fprintf(stderr, "Error Creating SDL Window. \n");
@@ -35,7 +42,12 @@ bool initialize_window(void)
 
 void setup(void)
 {
-	// TODO:
+	// Allocate memory for the array based on the window size with each element bing sized for uint32_t
+	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_height * window_width);
+
+	//Create SDL_Texture that will display the color buffer.
+	color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
+
 }
 
 void process_input(void)
@@ -63,12 +75,44 @@ void update(void)
 	// TODO:
 }
 
+void render_color_buffer(void)
+{
+	// Copy the color buffer into the SDL_Texture to be displayed.
+	SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)window_width * sizeof(uint32_t));
+	// Render SDL_Texture.
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+/*Color buffer is not actually displayed, it will be moved to an SDL_Texture to be displayed.*/
+void clear_color_buffer(uint32_t color)
+{
+	for (int y = 0; y < window_height; y++) //rows
+	{
+		for (int x = 0; x < window_width; x++) //columns
+		{
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
 void render(void)
 {
-	// TODO:
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
+
+	render_color_buffer();
+
+	clear_color_buffer(0xFFFFFF00);
+
 	SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void)
+{
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 int main(int argc, char* args[])
