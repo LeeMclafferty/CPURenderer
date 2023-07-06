c0b63336b0998a3cde328fb667529708d022c742
LeeMclafferty <lee.mclafferty@gmail.com>
2023-07-06 00:00:36 -0700

    Added rotation functions
        - Added functions to rotate points by x, y, or z.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
 vec3_t camera_pos = { 0, 0 , -5};
+vec3_t cube_rotation = { 0, 0, 0 };
 
 float fov_factor = 640;
 
@@ -38,10 +39,10 @@ void setup(void)
 	}
 }
 
-// Transform 3D vector to 2D vector/point scale location by fov
+// Transform 3D vector to 2D vector/point to render
 vec2_t project(vec3_t point)
 {
-	float x = (point.x * fov_factor) / point.z;
+	float x = (point.x * fov_factor) / point.z; // divide by z to mock scaling points
 	float y = (point.y * fov_factor) / point.z;
 	vec2_t projected_point = { .x = x, .y = y};
 	return projected_point;
@@ -69,15 +70,23 @@ void process_input(void)
 
 void update(void)
 {
+	cube_rotation.y += (float)0.0005;
+	cube_rotation.z += (float)0.0005;
+	cube_rotation.x += (float)0.0005;
+
 	for (int i = 0; i < N_POINTS; i++)
 	{
 		vec3_t point = cube_points[i];
 
-		// Move point away from camera to avoid the view port being in the center of our render.
-		point.z -= camera_pos.z;
+		vec3_t transformed_point = vec3_rotate_y(point, cube_rotation.y);
+		transformed_point = vec3_rotate_x(transformed_point, cube_rotation.x);
+		transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+		// Translate point away from camera to avoid the view port being in the center of our render.
+		transformed_point.z -= camera_pos.z;
 
 		// Project current point
-		vec2_t projected_point = project(point);
+		vec2_t projected_point = project(transformed_point);
 		projected_points[i] = projected_point;
 	}
 }
